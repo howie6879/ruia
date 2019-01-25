0a6bd90327939da838bc274ce293a74ac5d70cb8
howie6879 <xiaozizayang@gmail.com>
2019-01-25 11:25:42 +0800

    🎉 v0.4.0

--- examples/topics_examples/ruia_demo.py
+++ examples/topics_examples/ruia_demo.py
@@ -1,6 +1,5 @@
 from ruia import TextField, Item, Spider
 
-
 class HackerNewsItem(Item):
     target_item = TextField(css_select='tr.athing')
     title = TextField(css_select='a.storylink')
@@ -12,11 +11,6 @@ class HackerNewsSpider(Spider):
     async def parse(self, response):
         async for item in HackerNewsItem.get_items(html=response.html):
             yield item
-
-    async def save_item(self, item: HackerNewsItem):
-        # process item result data
-        pass
-
 
 if __name__ == '__main__':
     HackerNewsSpider.start()
