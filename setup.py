e7ca50d4171fdb37a8f7a1da0367a22223528858
howie6879 <xiaozizayang@gmail.com>
2019-07-12 10:32:00 +0800

    v0.6.1 🎉

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 
 from setuptools import find_packages, setup
 
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
+_version_re = re.compile(r"__version__\s+=\s+(.*)")
 
 PY_VER = sys.version_info
 
@@ -15,8 +15,8 @@ if PY_VER < (3, 6):
 
 
 def read_version():
-    regexp = re.compile(r"^__version__\W*=\W*'([\d.abrc]+)'")
-    init_py = os.path.join(os.path.dirname(__file__), 'ruia', '__init__.py')
+    regexp = re.compile(r'^__version__\W*=\W*"([\d.abrc]+)"')
+    init_py = os.path.join(os.path.dirname(__file__), "ruia", "__init__.py")
     with open(init_py) as f:
         for line in f:
             match = regexp.match(line)
@@ -30,38 +30,36 @@ def read(file_name):
 
 
 setup(
-    name='ruia',
+    name="ruia",
     version=read_version(),
-    author='Howie Hu',
+    author="Howie Hu",
     description="Async Python 3.6+ web scraping micro-framework based on asyncio.",
-    long_description=read('README.md'),
-    long_description_content_type='text/markdown',
-    author_email='xiaozizayang@gmail.com',
-    python_requires='>=3.6',
-    install_requires=['aiohttp>=3.5.4', 'cssselect', 'lxml'],
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    author_email="xiaozizayang@gmail.com",
+    python_requires=">=3.6",
+    install_requires=["aiohttp>=3.5.4", "cssselect", "lxml"],
     url="https://python-ruia.org/",
     packages=find_packages(),
-    license='MIT',
+    license="MIT",
     classifiers=[
-        'Framework :: AsyncIO',
-        'Intended Audience :: Developers',
+        "Framework :: AsyncIO",
+        "Intended Audience :: Developers",
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
         "Operating System :: POSIX :: Linux",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: POSIX :: BSD",
         "Operating System :: Microsoft :: Windows",
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Software Development :: Libraries :: Application Frameworks',
-        'Topic :: Software Development :: Libraries :: Python Modules',
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     project_urls={
-        'Documentation': 'https://docs.python-ruia.org/',
-        'Source': 'https://github.com/howie6879/ruia',
+        "Documentation": "https://docs.python-ruia.org/",
+        "Source": "https://github.com/howie6879/ruia",
     },
-    extras_require={
-        'uvloop': ['uvloop']
-    }
+    extras_require={"uvloop": ["uvloop"]},
 )
