9a5a226f7a6748c6edb49d3096085d0f892af531
howie6879 <howie6879@gmail.com>
2022-01-10 17:55:28 +0800

    Fixed: #149

--- setup.py
+++ setup.py
@@ -57,6 +57,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Software Development :: Libraries :: Application Frameworks",
         "Topic :: Software Development :: Libraries :: Python Modules",
