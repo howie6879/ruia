116e1cd01898495e85943b63903a973fe991708a
howie6879 <howie6879@gmail.com>
2022-01-10 17:54:58 +0800

    Fixed: #149

--- ruia/exceptions.py
+++ ruia/exceptions.py
@@ -2,28 +2,42 @@
 
 
 class IgnoreThisItem(Exception):
+    """Ignore Ruia's Item"""
+
     pass
 
 
 class InvalidCallbackResult(Exception):
+    """Get an invalid callback result"""
+
     pass
 
 
 class InvalidFuncType(Exception):
+    """Get an invalid function type"""
+
     pass
 
 
 class InvalidRequestMethod(Exception):
+    """Get an invalid request method"""
+
     pass
 
 
 class NotImplementedParseError(Exception):
+    """Get an invalid callback result"""
+
     pass
 
 
 class NothingMatchedError(Exception):
+    """Target match error"""
+
     pass
 
 
 class SpiderHookError(Exception):
+    """Spider hook function execution error"""
+
     pass
